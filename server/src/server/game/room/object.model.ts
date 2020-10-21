cc3cb17aa9b83731c196b40b31684ed1aeff36a0
Luis Ríos <36853390+lusor97@users.noreply.github.com>
2020-10-21 19:16:05 +0200

    Added inventory sistem
    Added inventory sistem

---
+++ server/src/server/game/room/object.model.ts
@@ -0,0 +1,6 @@
+export interface Object {
+    id: number;
+    name: string;
+    description: string;
+    quantity: number;
+  }
--- server/src/server/game/room/room.model.ts
+++ server/src/server/game/room/room.model.ts
@@ -1,6 +1,9 @@
+import { Object } from './object.model';
+
 export interface Room {
   id: number;
   state_code: number;
   text: string;
   options: string[];
+  inventary: Object[];
 }
